654b42ef63f73c2a44db0e98600c70fd538252bd
kotoshifof <kotoshifof@gmail.com>
2022-05-22 11:34:38 +0900

    🐛  Fix DB backup related error

--- django_project/weather/models.py
+++ django_project/weather/models.py
@@ -8,6 +8,6 @@ from django_project.users.models import User
 class City(models.Model):
     class Meta:
         db_table = 'city'
-        verbose_name = verbose_name_plural = '都市'
+        verbose_name = '都市'
     name = CharField(_("name"), max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
