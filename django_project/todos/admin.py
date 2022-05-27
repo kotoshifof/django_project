55feec7a597e8af69c4a19354d7c222af0b47ad7
kotoshifof <kotoshifof@gmail.com>
2022-05-27 19:04:40 +0900

    ✏️ Fix typos

--- django_project/todos/admin.py
+++ django_project/todos/admin.py
@@ -5,7 +5,7 @@ from .models import Todo
 
 
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'completed', 'ccreated_at', 'updated_at',)
+    list_display = ('name', 'completed', 'created_at', 'updated_at',)
 
 
 admin.site.register(Todo)
